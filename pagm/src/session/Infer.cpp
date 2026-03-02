98ac02faf435c41c09d22e9b624ecfe3a0feb89d
rangeset <2432647223@qq.com>
2026-03-02 13:07:00 +0800

    refactor(session): remove unnecessary functions

--- pagm/src/session/Infer.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026 Phiarc Team and St Rangeset
-// Licensed under the GNU General Public License v3.0
-// See LICENSE file for details.
-//
-// src/session/infer.cpp
-// Infer session.
-
-#include <session/Infer.hpp>
